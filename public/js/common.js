1874b7adb05bd12cb625db91901bb74f0deccb88
honeymaro <honeymaro@live.co.kr>
2018-11-05 18:21:48 +0900

    개발 환경을 구축한다

---
+++ public/js/common.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var calculateHeightFromWindow = function (el) {
+        var windowHeight = parseFloat($(window).height());
+        var elements = el ? el : $(".calHeight");
+
+        elements = elements.length ? elements : [elements];
+
+        var i = 0;
+
+        for (i = 0; i < elements.length; i++) {
+            var element = $(elements[i]);
+            var ratio = parseFloat($(element).attr("heightRatio"));
+            ratio = ratio ? ratio : 1;
+            $(element).css("height", windowHeight * ratio);
+        }
+    }
+
+    $(window).on("resize", function () {
+        calculateHeightFromWindow();
+
+    });
+
+    $(window).resize();
+})
